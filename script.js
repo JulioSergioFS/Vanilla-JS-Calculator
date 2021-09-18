bef094571fffa80dc38a3725af09343850bdfb47
Julio Sergio <juliosergio010302@gmail.com>
2021-09-18 11:53:05 -0300

    quick updates because the code wasn't fully working

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-const clear = document.getElementById('clear');
-const ce = document.getElementById('cancel-entry');
-const divide = document.getElementById('divide');
-const multiply = document.getElementById('multiply');
-const num = document.querySelectorAll('.button');
-const equal = document.getElementById('equal');
-const view = document.getElementById('viewfinder');
+const clear = document.getElementById("clear");
+const ce = document.getElementById("cancel-entry");
+const divide = document.getElementById("divide");
+const multiply = document.getElementById("multiply");
+const num = document.querySelectorAll(".number-button");
+const equal = document.getElementById("equal");
+const minus = document.getElementById("minus");
+const plus = document.getElementById("plus");
+const view = document.getElementById("viewfinder");
 
 clickSymbolButton(clear);
 clickSymbolButton(ce);
@@ -12,64 +14,43 @@ clickSymbolButton(divide);
 clickSymbolButton(multiply);
 clickNumberButton(num);
 clickSymbolButton(equal);
-pressKey();
-
+clickSymbolButton(plus);
+clickSymbolButton(minus);
 
 function clickNumberButton(button) {
-    button.forEach(value => {
-        value.addEventListener('click', () => {
-            view.innerHTML += value.textContent;
-        })
-    })
+  button.forEach((value) => {
+    value.addEventListener("click", () => {
+      view.innerHTML += value.textContent;
+    });
+  });
 }
 
 function clickSymbolButton(button) {
-    button.addEventListener('click', () => {
-        if (button.textContent === 'C') {
-            view.innerHTML = '';
-        }
+  button.addEventListener("click", () => {
+    if (button.textContent === "C") {
+      view.innerHTML = "";
+    }
 
-        if (button.textContent === 'CE') {
-            const viewArray = view.textContent.split('');
-            viewArray.pop()
-            view.innerHTML = viewArray.join('');
-        }
+    if (button.textContent === "CE") {
+      const viewArray = view.textContent.split("");
+      viewArray.pop();
+      view.innerHTML = viewArray.join("");
+    }
 
-        if (button.textContent === '÷') {
-            view.innerHTML += '/';
-        }
+    if (button.textContent === "÷") {
+      view.innerHTML += "/";
+    }
 
-        if (button.textContent === 'X') {
-            view.innerHTML += '*';
-        }
+    if (button.textContent === "x") {
+      view.innerHTML += "*";
+    }
 
-        if (button.textContent === '=') {
-            view.innerHTML = eval(view.innerHTML);
-        }
-    })
-}
+    if (button.textContent === "-" || button.textContent === "+") {
+      view.innerHTML += button.textContent;
+    }
 
-function pressKey() {
-    document.addEventListener('keydown', e => {
-        if (!isNaN(parseInt(e.key, 10))) {
-            view.innerHTML += e.key;
-        } else {
-            if (e.key.toUpperCase() === 'C') {
-                view.innerHTML = '';
-            }
-
-            if (new Array('/', '*', '+', '-').includes(e.key)) {
-                view.innerHTML += e.key;
-            }
-
-            if (e.key.toUpperCase() === 'X') {
-                view.innerHTML += '*';
-            }
-
-            if (e.key === '=' || e.key === 'Enter') {
-                view.innerHTML = eval(view.innerHTML);
-            }
-        }
-    })
-
+    if (button.textContent === "=") {
+      view.innerHTML = eval(view.innerHTML);
+    }
+  });
 }
